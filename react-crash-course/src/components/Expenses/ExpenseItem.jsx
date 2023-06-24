ff0fd106f3bb1a1eb1a1e2f991441ca4266bbfe1
YoungHan Jo <verin4494@gmail.com>
2023-06-24 12:35:49 +0900

    form

--- react-crash-course/src/components/Expenses/ExpenseItem.jsx
+++ react-crash-course/src/components/Expenses/ExpenseItem.jsx
@@ -1,17 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import ExpenseDate from './ExpenseDate';
 import Card from '../UI/Card';
 import './ExpenseItem.css';
 
-const ExpenseItem = ({ date, title, amount }) => {
+const ExpenseItem = props => {
+  // Reactは最初にDOMレンダリングをしてから二回目はレンダリングしない。
+  // しかし、useStateを使って状態を変更すると、そのコンポーネント関数を再実行する。
+  // Reactフックは関数型コンポーネントに直接使わなければならない。
+  // 全てのstateは独立的である。
+  const [title, setTitle] = useState(props.title); // 初期化は最初一回だけ行われる。// stateが変わった時は、初期化しない。
+  const clickHandler = () => {
+    setTitle('Updated!'); // これを呼び出すと、Reactはこのコンポーネントを再実行する。
+    console.log(title);
+  };
+
   return (
     <Card className="expense-item">
-      <ExpenseDate date={date} />
+      <ExpenseDate date={props.date} />
       <div className="expense-item__description">
         <h2>{title}</h2>
-        <div className="expense-item__price">${amount}</div>
+        <div className="expense-item__price">${props.amount}</div>
       </div>
+      <button onClick={clickHandler}>Change Title</button>
     </Card>
   );
 };
---
+++ react-crash-course/src/components/NewExpense/ExpenseForm.jsx
@@ -0,0 +1,79 @@
+import React, { useState } from 'react';
+
+import './ExpenseForm.css';
+
+const ExpenseForm = () => {
+  const [enteredTitle, setEnteredTitle] = useState('');
+  const [enteredAmount, setEnteredAmount] = useState('');
+  const [enteredDate, setEnteredDate] = useState('');
+  //   const [userInput, setUserInput] = useState({
+  //     enteredTitle: '',
+  //     enteredAmount: '',
+  //     enteredDate: '',
+  //   });
+
+  const titleChangeHandler = event => {
+    setEnteredTitle(event.target.value);
+    // setUserInput({
+    //   ...userInput,　// userInputをそのまま使うと、まだ更新されてないものを使う可能性もある。//stateの更新のスケジュールがすぐに行われないから
+    //   enteredTitle: event.target.value,
+    // });
+    // ＊＊＊＊＊＊＊＊＊＊以前のstateに依存するときは下記のcallback方法を使うこと！＊＊＊＊＊＊＊＊＊＊＊
+    // setUserInput(prevState => {
+    //   //callback functionを使うと、最新のstateを使うことができる。
+    //   return { ...prevState, enteredTitle: event.target.value };
+    // });
+  };
+
+  const amountChangeHandler = event => {
+    setEnteredAmount(event.target.value);
+  };
+
+  const dateChangeHandler = event => {
+    setEnteredDate(event.target.value);
+  };
+
+  const submitHandler = event => {
+    event.preventDefault();
+    const expenseData = {
+      title: enteredTitle,
+      amount: enteredAmount,
+      date: new Date(enteredDate),
+    };
+    console.log(expenseData);
+  };
+
+  return (
+    <form onSubmit={submitHandler}>
+      <div className="new-expense__controls">
+        <div className="new-expense__control">
+          <label>Title</label>
+          <input type="text" onChange={titleChangeHandler} />
+        </div>
+        <div className="new-expense__control">
+          <label>Amount</label>
+          <input
+            type="number"
+            min="0.01"
+            step="0.01"
+            onChange={amountChangeHandler}
+          />
+        </div>
+      </div>
+      <div className="new-expense__control">
+        <label>Date</label>
+        <input
+          type="date"
+          min="2020-01-01"
+          max="2023-12-31"
+          onChange={dateChangeHandler}
+        />
+      </div>
+      <div className="new-expense_actions">
+        <button type="submit">Add Expense</button>
+      </div>
+    </form>
+  );
+};
+
+export default ExpenseForm;
