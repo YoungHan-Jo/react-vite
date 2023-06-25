e7b765b9dafb75fc32b3e2ee4d6438c40a0737e0
YoungHan Jo <verin4494@gmail.com>
2023-06-25 23:16:01 +0900

    css module

--- 01-starting-project 2/src/components/CourseGoals/CourseInput/CourseInput.jsx
+++ 01-starting-project 2/src/components/CourseGoals/CourseInput/CourseInput.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 
 import Button from "../../UI/Button/Button";
-import "./CourseInput.css";
+import styles from "./CourseInput.module.css";
 
 const CourseInput = (props) => {
   const [enteredValue, setEnteredValue] = useState("");
@@ -28,7 +28,10 @@ const CourseInput = (props) => {
 
   return (
     <form onSubmit={formSubmitHandler}>
-      <div className={`form-control ${!isValid ? "invalid" : ""}`}>
+      <div
+        className={`${styles["form-control"]} ${!isValid && styles.invalid}`}
+      >
+        {/* // {`form-control ${!isValid ? "invalid" : ""}`}> */}
         <label>Course Goal</label>
         <input
           type="text"
