8fc97fe4eabb698d15106941a915416821bc3d2a
YoungHan Jo <verin4494@gmail.com>
2023-06-30 16:02:26 +0900

    react-dom Portal

--- practice1/src/App.jsx
+++ practice1/src/App.jsx
@@ -1,8 +1,8 @@
-import React, { useState } from 'react';
+import React from 'react';
 
+import './App.css';
 import NewUser from './components/NewUser/NewUser';
 import UsersList from './components/Users/UsersList';
-import './App.css';
 
 function App() {
   return (
