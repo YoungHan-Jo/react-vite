faf8ae08b6d06befefc2cbe9bb7b9968e3fd4833
YoungHan Jo <verin4494@gmail.com>
2023-07-01 11:37:10 +0900

    useReducer + useEffectでform作成

--- useEffect-study/src/components/Login/Login.js
+++ useEffect-study/src/components/Login/Login.js
@@ -28,7 +28,7 @@ const passwordReducer = (state, action) => {
 
 const Login = props => {
   // =======useReducer使用　start=======
-  const [formIsValid, setFormIsValid] = useState(false);
+  const [formIsValid, setFormIsValid] = useState(false); // form有効性を他のstateから導出してるから、最新のstateじゃない可能性もある。＝＞ useEffectで解決できる。
 
   const [emailState, dispatchEmail] = useReducer(emailReducer, {
     value: '',
@@ -40,16 +40,29 @@ const Login = props => {
     isValid: null,
   });
 
+  // 最初のrenderの時は変わってと認識して、useEffectが実行される。
+  useEffect(() => {
+    const identifier = setTimeout(() => {
+      // console.log('Checking form validity!');
+      setFormIsValid(emailState.isValid && passwordState.isValid);
+    }, 500);
+
+    // cleanup functionは、次のeffectが実行される前に実行される。
+    return () => {
+      // console.log('EFFECT CLEANUP');
+      clearTimeout(identifier); // 直前のtimeoutをclearする。=> 最終的には、最後のtimeoutだけが実行される。
+    };
+  }, [emailState.isValid, passwordState.isValid]); // このdependency arrayが変わった時だけ実行される。
+  // formIsValidは　emailStateとpasswordStateのisvalidだけ必要
+
   const emailChangeHandler = event => {
     dispatchEmail({ type: 'USER_INPUT', val: event.target.value }); // dispatchは、action objectを受け取る。
 
-    setFormIsValid(event.target.value.includes('@') && passwordState.isValid);
+    // setFormIsValid(event.target.value.includes('@') && passwordState.isValid);
   };
 
   const passwordChangeHandler = event => {
     dispatchPassword({ type: 'PASSWORD_INPUT', val: event.target.value });
-
-    setFormIsValid(emailState.isValid && event.target.value.trim().length > 6);
   };
 
   const validateEmailHandler = () => {
@@ -59,7 +72,6 @@ const Login = props => {
   };
 
   const validatePasswordHandler = () => {
-    // setPasswordIsValid(enteredPassword.trim().length > 6);
     dispatchPassword({ type: 'PASSWORD_BLUR' });
   };
 
@@ -67,51 +79,6 @@ const Login = props => {
     event.preventDefault();
     props.onLogin(emailState.value, passwordState.value);
   };
-  // ======useReducer使用　end=======
-
-  // // ==========useEffect使用　start==========
-  // const [enteredEmail, setEnteredEmail] = useState('');
-  // const [emailIsValid, setEmailIsValid] = useState();
-  // const [enteredPassword, setEnteredPassword] = useState('');
-  // const [passwordIsValid, setPasswordIsValid] = useState();
-  // const [formIsValid, setFormIsValid] = useState(false);
-
-  // useEffect(() => {
-  //   const identifier = setTimeout(() => {
-  //     // console.log('Checking form validity!');
-  //     setFormIsValid(
-  //       enteredEmail.includes('@') && enteredPassword.trim().length > 6,
-  //     );
-  //   }, 500);
-
-  //   // cleanup functionは、次のeffectが実行される前に実行される。
-  //   return () => {
-  //     // console.log('CLEANUP');
-  //     clearTimeout(identifier); // 直前のtimeoutをclearする。=> 最終的には、最後のtimeoutだけが実行される。
-  //   };
-  // }, [enteredEmail, enteredPassword]); // このdependency arrayが変わった時だけ実行される。
-
-  // const emailChangeHandler = event => {
-  //   setEnteredEmail(event.target.value);
-  // };
-
-  // const passwordChangeHandler = event => {
-  //   setEnteredPassword(event.target.value);
-  // };
-
-  // const validateEmailHandler = () => {
-  //   setEmailIsValid(enteredEmail.includes('@'));
-  // };
-
-  // const validatePasswordHandler = () => {
-  //   setPasswordIsValid(enteredPassword.trim().length > 6);
-  // };
-
-  // const submitHandler = event => {
-  //   event.preventDefault();
-  //   props.onLogin(enteredEmail, enteredPassword);
-  // };
-  // // =========useEffect使用　end==========
 
   return (
     <Card className={classes.login}>
