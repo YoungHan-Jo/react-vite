90b5f52a186b1918b04fde24ad6e0fc3e2d1b8be
YoungHan Jo <verin4494@gmail.com>
2023-07-05 00:29:01 +0900

    fowardRefとuseImperativeHandleを利用して外部でfocusを使う

--- useEffect-study/src/components/UI/Input/Input.js
+++ useEffect-study/src/components/UI/Input/Input.js
@@ -1,8 +1,22 @@
-import React from 'react';
+import React, { useRef, useImperativeHandle } from 'react';
 
 import classes from './Input.module.css';
 
-const Input = props => {
+// forwardRefを使って、refを受け取れるようにする。
+const Input = React.forwardRef((props, ref) => {
+  const inputRef = useRef();
+
+  const activate = () => {
+    inputRef.current.focus();
+  };
+
+  // funcionを外部で使えるようにする。
+  useImperativeHandle(ref, () => {
+    return {
+      focus: activate, // focusは、外部で使える名前
+    };
+  });
+
   return (
     <div
       className={`${classes.control} ${
@@ -11,6 +25,7 @@ const Input = props => {
     >
       <label htmlFor={props.id}>{props.label}</label>
       <input
+        ref={inputRef}
         type={props.type}
         id={props.id}
         value={props.value}
@@ -19,6 +34,6 @@ const Input = props => {
       />
     </div>
   );
-};
+});
 
 export default Input;
