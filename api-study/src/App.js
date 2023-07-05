d9adfc6f39613df0a543ab08b625360d4c82ac83
YoungHan Jo <verin4494@gmail.com>
2023-07-05 23:52:04 +0900

    http API get

---
+++ api-study/src/App.js
@@ -0,0 +1,40 @@
+import React, { useState } from "react";
+
+import MoviesList from "./components/MoviesList";
+import "./App.css";
+
+function App() {
+  const [movies, setMovies] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const fetchMovesHandler = async () => {
+    setIsLoading(true);
+    const response = await fetch("https://swapi.dev/api/films");
+    const data = await response.json();
+    const transformedMovies = data.results.map((movie) => {
+      return {
+        id: movie.episode_id,
+        title: movie.title,
+        releaseDate: movie.release_date,
+        openingText: movie.opening_crawl,
+      };
+    });
+    setMovies(transformedMovies);
+    setIsLoading(false);
+  };
+
+  return (
+    <React.Fragment>
+      <section>
+        <button onClick={fetchMovesHandler}>Fetch Movies</button>
+      </section>
+      <section>
+        {!isLoading && movies.length > 0 && <MoviesList movies={movies} />}
+        {isLoading && <p>Loading ...</p>}
+        {!isLoading && movies.length === 0 && <p>Found no movies.</p>}
+      </section>
+    </React.Fragment>
+  );
+}
+
+export default App;
